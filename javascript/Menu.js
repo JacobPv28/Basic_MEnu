8c2ca40221a9a003f5cd9c23d8ff0c9ae96e18e3
Jacobbo Parra Volveras <109887915+Spieluhr661@users.noreply.github.com>
2023-01-30 17:34:57 -0800

    Update Menu.js

--- javascript/Menu.js
+++ javascript/Menu.js
@@ -2,8 +2,7 @@ import { ShowPrice } from "./module/ShowPrice.js";
 
 ShowPrice();
 
-
-
+// Durante el proceso intente separar la funcion del menu que tiene el accionar del boton con el switch, pero no me funcionaba, asi que deje tanto la funcion que reemplaza lo que se muestra y el menu en un solo modulo.
 
 
 
